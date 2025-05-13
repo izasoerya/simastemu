c64a523f85e05ba3af5e591ac8eeba4597a37b56
izasoerya <ihza.surya@gmail.com>
2025-05-13 19:40:28 +0700

    feat: inventory template

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { DbModule } from './db/db.module';
+import { InventoryModule } from './inventory/inventory.module';
 
 @Module({
-  imports: [AuthModule, DbModule],
+  imports: [AuthModule, DbModule, InventoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
