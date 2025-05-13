7f9d34ddde00768c81ca27e2308f363d63214c44
izasoerya <ihza.surya@gmail.com>
2025-05-13 17:21:45 +0700

    feat: enforce dto on reutrn

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,18 @@
-import { Body, Controller, Get, Post, Request } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UserSignInDto, UserSignUpDto } from './dtos/auth.dto';
+import { ResponseAuthDto, UserSignInDto, UserSignUpDto } from './dtos/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Get('sign-in')
-  signIn(@Body() UserSignInDto: UserSignInDto) {
+  signIn(@Body() UserSignInDto: UserSignInDto): Promise<ResponseAuthDto> {
     return this.authService.userSignIn(UserSignInDto);
   }
 
   @Post('sign-up')
-  signUp(@Body() UserSignUpDto: UserSignUpDto) {
+  signUp(@Body() UserSignUpDto: UserSignUpDto): Promise<ResponseAuthDto> {
     return this.authService.userSignUp(UserSignUpDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,25 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { ResponseAuthDto, UserSignInDto, UserSignUpDto } from './dtos/auth.dto';
 import { UserService } from '../user/user.service';
 import { CreateUserDto } from '../user/dtos/user.dto';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UserService) {}
 
   async userSignIn(user: UserSignInDto): Promise<ResponseAuthDto> {
-    const hashedPassword = 'later will be fixed >.<';
     const loggedUser = await this.userService.findOne(
       user.email,
-      hashedPassword,
+      user.password,
     );
-    return loggedUser;
+    const res: ResponseAuthDto = {
+      uid: loggedUser.uid,
+      name: loggedUser.name,
+      email: loggedUser.email,
+      createdAt: loggedUser.createdAt,
+    };
+    return res;
   }
 
   async userSignUp(user: UserSignUpDto): Promise<ResponseAuthDto> {
-    const hashedPassword = 'later will be fixed >.<';
     const createUserDto: CreateUserDto = {
       ...user,
-      password_hashed: hashedPassword,
     };
 
     const newUser = await this.userService.createUser(createUserDto);
