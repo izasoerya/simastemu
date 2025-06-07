f6814d6573308e9367db1c946095b3c926ededcf
izasoerya <ihza.surya@gmail.com>
2025-06-07 21:18:24 +0700

    fix: path correction

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -6,7 +6,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV || 'development'}.local`,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
     }),
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
