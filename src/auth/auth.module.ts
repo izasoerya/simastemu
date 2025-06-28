6a22a97b93750ef424ec0f0877b77d6009d7241e
izasoerya <ihza.surya@gmail.com>
2025-06-28 20:39:27 +0700

    feat: move constant to env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,13 +4,18 @@ import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtKey } from './constants';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
     JwtModule.register({
       global: true,
-      secret: jwtKey.secret,
+      secret: process.env.JWT_KEY,
       signOptions: { expiresIn: '3600s' },
     }),
   ],
