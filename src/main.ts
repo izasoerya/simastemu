d69e42383a6937f51da57cdccca2e4c15a918f47
izasoerya <ihza.surya@gmail.com>
2025-07-30 14:28:11 +0700

    feat: redirect unproper request to badrequest

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log'],
   });
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT!);
 }
 
