a313f6a8c01350b764bf044fd30c40da6a418e2d
izasoerya <ihza.surya@gmail.com>
2026-03-21 22:38:14 +0700

    feat: swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log'],
   });
+
+  app.use(
+    '/docs',
+    basicAuth({
+      users: {
+        [process.env.DOCS_USER || 'admin']:
+          process.env.DOCS_PASSWORD || 'password',
+      },
+      challenge: true,
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('SimasetMU OpenAPI')
+    .setDescription('API description for testing')
+    .setVersion('1.0')
+    .addBasicAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
