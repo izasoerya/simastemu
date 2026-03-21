7cad304d9f691e420079a55277ae26a92d6c1618
izasoerya <ihza.surya@gmail.com>
2026-03-21 18:17:54 +0700

    chore: new migration head

---
+++ src/migrations/1774091758761-init.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Init1774091758761 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
