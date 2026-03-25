ac241ab6b9ad3e351a47b7caa2116a64b40c76d9
izasoerya <ihza.surya@gmail.com>
2026-03-25 21:42:47 +0700

    rm: otp controller since it just service

--- src/otp/otp.module.ts
+++ src/otp/otp.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OtpService } from './otp.service';
-import { OtpController } from './otp.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OTP } from './entities/otp.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([OTP])],
-  controllers: [OtpController],
   providers: [OtpService],
   exports: [OtpService],
 })
